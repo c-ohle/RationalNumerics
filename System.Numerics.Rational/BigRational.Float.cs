14492af7d009c6dbc2ec81be68f56945279ab7d8
Christian Ohle <c.ohle@outlook.com>
2022-08-12 00:06:00 +0200

    floats

--- System.Numerics.Rational/BigRational.Float.cs
+++ System.Numerics.Rational/BigRational.Float.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 
 namespace System.Numerics
 {
-  unsafe partial struct BigRational   
+  unsafe partial struct BigRational
   {
     /// <summary>
     /// Represents a sizeof(<typeparamref name="T"/>)-precision-floating floating-point number. (under construction)
@@ -21,12 +21,17 @@ namespace System.Numerics
     /// <c>BigRational.Float&lt;Int256&gt;</c> defines a Octuple precision type.<br/>
     /// </remarks>
     /// <typeparam name="T"></typeparam>
-    [Serializable, SkipLocalsInit] 
+    [Serializable, SkipLocalsInit, DebuggerDisplay("{ToString(),nq}")]
     public unsafe readonly struct Float<T> where T : unmanaged
     {
       public override readonly string ToString()
       {
-        return ((BigRational)this).ToString(sizeof(T) == 2 ? "L4" : sizeof(T) == 4 ? "L7" : sizeof(T) == 8 ? "L15" : sizeof(T) == 16 ? "L34" : "L71");
+        return ((BigRational)this).ToString(
+          sizeof(T) == 2 ? "L4" :
+          sizeof(T) == 4 ? "L7" :
+          sizeof(T) == 8 ? "L15" :
+          sizeof(T) == 12 ? "L25" :
+          sizeof(T) == 16 ? "L34" : "L71");
       }
 
       public static implicit operator Float<T>(int value)
@@ -36,15 +41,10 @@ namespace System.Numerics
       }
       public static implicit operator Float<T>(double value)
       {
-        if (sizeof(T) == sizeof(double)) return *(Float<T>*)&value;
-        var cpu = main_cpu; var e = Float<double>.exp((uint*)&value);
-        Float<double>.push(cpu, (uint*)&value);
+        if (sizeof(T) == 8) return *(Float<T>*)&value;
+        var cpu = main_cpu; Float<double>.push(cpu, (uint*)&value); 
+        var e = Float<double>.exp((uint*)&value);
         Float<T> c; ctor(cpu, e, (uint*)&c); return c;
-      }
-      public static implicit operator BigRational(Float<T> value)
-      {
-        var cpu = main_cpu; var p = (uint*)&value.p;
-        push(cpu, p); cpu.pow(2, exp(p)); cpu.mul(); return cpu.popr();
       }
       public static explicit operator Float<T>(BigRational value)
       {
@@ -55,12 +55,30 @@ namespace System.Numerics
         cpu.shl(v); cpu.swp(); cpu.idiv();
         Float<T> c; ctor(cpu, k - j - m, (uint*)&c); return c;
       }
-                                       
+
+      public static explicit operator int(Float<T> value)
+      {
+        return (int)(double)value;
+      }
+      public static explicit operator double(Float<T> value)
+      {
+        if (sizeof(T) == 8) return *(double*)&value;
+        var cpu = main_cpu; push(cpu, (uint*)&value); var e = exp((uint*)&value);
+        Float<double> c; Float<double>.ctor(cpu, e, (uint*)&c); return *(double*)&c;
+      }
+      public static implicit operator BigRational(Float<T> value)
+      {
+        var cpu = main_cpu; var p = (uint*)&value.p;
+        push(cpu, p); cpu.pow(2, exp(p)); cpu.mul(); return cpu.popr();
+      }
+
       public static Float<T> operator +(Float<T> a) => a;
       public static Float<T> operator -(Float<T> a)
       {
-        ushort* u = (ushort*)&a, h = &u[(sizeof(T) >> 1) - 1];
-        if (*h != 0) *h ^= 0x8000; return a;
+        uint* u = (uint*)&a;
+        if ((sizeof(T) & 2) == 0) u[lui] ^= 0x80000000; else u[lui] ^= 0x8000;
+        //if (u[lui] != 0) u[lui] ^= 0x80000000; 
+        return a;
       }
       public static Float<T> operator +(Float<T> a, Float<T> b)
       {
@@ -69,8 +87,9 @@ namespace System.Numerics
       }
       public static Float<T> operator -(Float<T> a, Float<T> b)
       {
-        ushort* u = (ushort*)&a, v = (ushort*)&b, h = &v[(sizeof(T) >> 1) - 1];
-        Float<T> c; *h ^= 0x8000; add((uint*)u, (uint*)v, (uint*)&c); return c;
+        uint* u = (uint*)&a, v = (uint*)&b;
+        if ((sizeof(T) & 2) == 0) v[lui] ^= 0x80000000; else v[lui] ^= 0x8000;
+        Float<T> c; add(u, v, (uint*)&c); return c;
       }
       public static Float<T> operator *(Float<T> a, Float<T> b)
       {
@@ -96,54 +115,78 @@ namespace System.Numerics
         Float<T> c; ctor(cpu, 0, (uint*)&c); return c;
       }
 
+      //[DebuggerBrowsable(DebuggerBrowsableState.Never)]
       private readonly T p;
       private static int exp(uint* p)
       {
-        return unchecked((int)((p[sizeof(T) != 2 ? (sizeof(T) >> 2) - 1 : 0] >> esh) & msk)) - bia;
+        return unchecked((int)((p[lui] >> esh) & msk)) - bia;
       }
       private static void push(CPU cpu, uint* p)
       {
-        var l = sizeof(T) != 2 ? (sizeof(T) >> 2) - 1 : 0;
-        if ((sizeof(T) != 2 ? p[l] : (p[l] & 0xffff)) == 0) { cpu.push(); return; }
+        var l = lui; if (((sizeof(T) & 2) == 0 ? p[l] : (p[l] & 0xffff)) == 0) { cpu.push(); return; }
         var t = stackalloc uint[l + 4]; var x = 1u << (mbi - 1);
-        t[0] = (sizeof(T) != 2 ? p[l] & 0x80000000 : (p[l] & 0x8000) << 16) | unchecked((uint)l + 1);
+        if ((sizeof(T) & 2) != 0) if (l != 0) p[l] <<= 16; else p[0] = (p[0] & 0x03FFu) | ((p[0] & ~0x03FFu) << 16);
+        t[0] = (p[l] & 0x80000000) | unchecked((uint)l + 1);
         t[l + 1] = (p[l] & (x - 1)) | x; t[l + 2] = t[l + 3] = 1;
         for (int i = 0; i < l; i++) t[i + 1] = p[i];
-        cpu.push(new ReadOnlySpan<uint>(t, l + 4));
+        cpu.push(new ReadOnlySpan<uint>(t, l + 4)); //return unchecked((int)((p[lui] >> esh) & msk)) - bia;
       }
       private static void ctor(CPU cpu, int e, uint* p)
       {
         var s = unchecked((int)cpu.msb()); if (s == 0) { cpu.pop(); *(Float<T>*)p = default; return; }
         var d = s - mbi; cpu.shr(d); cpu.get(cpu.mark() - 1, out ReadOnlySpan<uint> sp);
-        var l = sizeof(T) != 2 ? (sizeof(T) >> 2) - 1 : 0; var bi = unchecked((int)mbi - 1);
+        var l = lui; var bi = unchecked((int)mbi - 1);
         var f = (unchecked((uint)((e + d) + bia)) << esh) | (sp[l + 1] & ((1u << bi) - 1));
-        if (sizeof(T) != 2) p[l] = (sp[0] & 0x80000000) | f;
-        else ((ushort*)p)[0] = unchecked((ushort)(((sp[0] & 0x80000000) >> 16) | f));
+        p[l] = (sp[0] & 0x80000000) | f;
+        if ((sizeof(T) & 2) != 0) { if (l != 0) p[l] >>= 16; else p[l] = (p[l] & 0xffff) | p[l] >> 16; }
         for (int i = 0; i < l; i++) p[i] = sp[i + 1];
         cpu.pop();
       }
       private static void add(uint* u, uint* v, uint* c)
       {
-        int ae = exp(u), be = exp(v), e, l = ae < be ? 1 : 0;
         var cpu = main_cpu; push(cpu, u); push(cpu, v);
+        int ae = exp(u), be = exp(v), e, l = ae < be ? 1 : 0;
         cpu.shr(l == 0 ? (e = ae) - be : (e = be) - ae, l);
         cpu.add(); ctor(cpu, e, c);
       }
-      [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-      private static readonly int mbi, bia, esh, msk;
+      //[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+      private static readonly int sbi, mbi, bia, esh, msk, lui;
       static Float()
       {
         switch (sizeof(T))
         {
-          case 02: mbi = 011; esh = 10; msk = 0x01f; bia = 0025; break;
-          case 04: mbi = 024; esh = 23; msk = 0x0ff; bia = 0150; break;
-          case 08: mbi = 053; esh = 20; msk = 0x7ff; bia = 1075; break;
-          //case 10: mbi = 113; esh = 20; msk = 0x7ff; bia = 1075; break; //todo: 80 bit extended 
-          case 16: mbi = 113; esh = 20; msk = 0x7ff; bia = 1075; break; //todo: check
-          case 32: mbi = 237; esh = 20; msk = 0x7ff; bia = 1075; break; //todo: check
-          default: throw new NotSupportedException($"{typeof(T)} {sizeof(T) << 3}"); //todo: intermediate sizes
+          case 02: sbi = 05; break; // Half 16 bit
+          case 04: sbi = 08; break; // Single 32 bit
+          case 08: sbi = 11; break; // Double 64 bit
+          case 10: sbi = 15; break; // Extended 80 bit
+          case 12: sbi = 15; break; // Extended 96 bit
+          case 16: sbi = 15; break; // Quadruple 128 bit
+          case 32: sbi = 19; break; // Octuple 256
+          default: throw new NotSupportedException($"{typeof(T)} {sizeof(T) << 3}"); //todo: f() intermediate sizes
+        }
+        msk = (1 << sbi) - 1;
+        esh = 32 - sbi - 1; // (sizeof(T) != 2 ? 32 : 16) - sbi - 1;
+        mbi = (sizeof(T) << 3) - sbi;
+        bia = (1 << (sbi - 1)) + mbi - 2;
+        lui = (sizeof(T) >> 2) - 1;
+        if ((sizeof(T) & 2) != 0) { lui++; }
+        if ((sizeof(T)) == 2)
+        {
+
         }
       }
+
+      internal static int BiasedExponentShift => mbi - 1; // dbl 52
+      internal static int MinBiasedExponent => 0;
+      internal static int MaxBiasedExponent => msk; // dbl 0x07FF;
+      internal static int ExponentBias => (1 << (sbi - 1)) - 1; // dbl 1023
+      internal static int MinExponent => 1 - ExponentBias; // dbl -1022;
+      internal static int MaxExponent => ExponentBias; // dbl +1023;
+      internal static int Bits => sizeof(T) << 3;
+
+      public static Float<T> MinValue { get { return 0; } }
+      public static Float<T> MaxValue { get { return 0; } }
+
     }
 
   }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,17 @@
 ﻿global using System.Diagnostics;
 global using System.Numerics;
 global using System.Numerics.Rational;
+
 global using rat = System.Numerics.BigRational;
 global using BigInt = System.Numerics.BigRational.Integer;
 global using float16 = System.Numerics.BigRational.Float<System.Int16>;
 global using float32 = System.Numerics.BigRational.Float<System.Int32>;
 global using float64 = System.Numerics.BigRational.Float<System.Int64>;
+global using float80 = System.Numerics.BigRational.Float<Test._base80>;
+global using float96 = System.Numerics.BigRational.Float<Test._base96>;
 global using float128 = System.Numerics.BigRational.Float<(System.Int64, System.Int64)>; //System.Int128 for NET7
+global using float256 = System.Numerics.BigRational.Float<(System.Int64, System.Int64, System.Int64, System.Int64)>; //System.Int128 for NET7
+using System.Runtime.InteropServices;
 
 namespace Test
 {
@@ -21,19 +26,29 @@ namespace Test
 
     static void test()
     {
-      float128 a, b, c;
-      a = Math.PI;
-      b = (float128)BigRational.Pi(38);
-      a = b;
-      b = a * a;
-      c = b / a;
-      c = a + a;
-      b = c - a;
-      a = a + 100000;
-      a = a - 100000;
-      a = a - 100000;
-      a = a + 100000;
+      var a = (float128)1; a /= 3;
+      var b = (float128)BigRational.Pi(100);
+      var c = b * 100;
+      c = c / 100;
+      c = c + 100;
+      c = c - 100;
+
+      c = b * 1234.5678;
+      c = c / 1234.5678;
+      c = c + 1234.5678;
+      c = c - 1234.5678;
+
+      c = b * 128;
+      c = c / 128;
+      c = c + 128;
+      c = c - 128;
+
     }
   }
+  //unsafe struct _base80 { fixed ushort a[5]; }
+  [StructLayout(LayoutKind.Sequential, Pack = 2, Size = 10)]
+  unsafe struct _base80 { ushort h; ulong l; }
+  [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 12)]
+  unsafe struct _base96 { uint h; ulong l; }
 
 }
